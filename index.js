9281e1207614120533bb6115515648239155604b
MinhDV <minhdv@ominext.com>
2019-12-13 11:08:27 +0700

    demo get request

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var expres = require('express');
 var app = expres();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var request = require('request');
 
 app.use(expres.static('public'));
 app.set('view engine', 'ejs');
-
-server.listen(2000);
-
-io.on('connection', function (socket) {
-    //
-});
+app.listen(2020);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html')
-});
+    request('https://www.ominext.com/', function (err, response, body) {
+        if (err) {
+            throw err;
+        } else {
+            res.render('index', {html: body});
+        }
+    });
 
-// app.get('/', function (req, res) {
-//    res.render('index');
-// });
+});
