8fa2e7748a176f748349358453e23d5951d0f981
MinhDV <minhdv@ominext.com>
2019-12-17 13:46:03 +0700

    demo middleware

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 var expres = require('express');
 var app = expres();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
 app.use(expres.static('public'));
-app.set('view engine', 'ejs');
 
-server.listen(2000);
+app.listen(2000);
 
-io.on('connection', function (socket) {
-    //
+// Đây là middleware cấp ứng dụng
+// khi có request /user/:id thì nó sẽ thực hiện function và trả về text là "USER"
+app.get('/user/:id', function (req, res, next) {
+    res.send('USER');
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html')
+
+// Tương tự như middleware trên, sau khi thực hiện xong chức năng ở function đầu tiên
+// Nó sẽ chạy hàm next() và chuyển đến thực hiện chức năng ở function tiếp theo.
+app.use('/user/:id', function (req, res, next) {
+    console.log('Request URL:', req.originalUrl)
+    next()
+  }, function (req, res, next) {
+    console.log('Request Type:', req.method)
+    next()
 });
 
-// app.get('/', function (req, res) {
-//    res.render('index');
-// });
+
+// Sử dụng hàm next('route') để bỏ qua các middleware cần thực hiện
+// Ví dụ, khi có request /user/:id thì nó sẽ thực hiện function
+app.get('/book/:id', function first(req, res, next) {
+    // Nếu tham số id = 0 thì sẽ thực hiện hàm next('router') bỏ qua function second() và chạy function last()
+    if (req.params.id === '0') next('route')
+    // Ngược lại nếu id khác 0 thì sẽ chạy đến function second
+    else next();
+  }, function second(req, res, next) {
+    // in ra text second
+    res.send('second');
+});
+// hàm này chỉ được thực hiện khi id = 0
+app.get('/book/:id', function last(req, res, next) {
+    res.send('last');
+});
+
+
+// Ví dụng về middleware error-handling
+app.get('/users/:id', function(req, res, next){
+    // Nếu id = 0
+    if (req.params.id == 0) {
+        // khai báo object error với các thông tin lỗi
+        let error = {
+            title: "Không tìm thấy ID",
+            httpStatusCode: 400
+        }
+        // Chuyển object error đến middleware xử lý lỗi để xử lý
+        return next(error);
+    }
+    // Nếu id khác 0 thì in ID ra màn hình
+    res.end('ID: ' + req.params.id);
+});
+// Đây là middleware xử lý lỗi, sẽ tiếp nhận object error và trả kết quả cho client
+app.use(function(err, req, res, next){
+    // Trả về status:400 và in text ra màn hình là Không tìm thấy ID
+    res.status(err.httpStatusCode).send(err.title);
+});
