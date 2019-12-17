fae3135c4cc61cbf8ac047d842a4934a12618470
MinhDV <minhdv@ominext.com>
2019-12-17 18:05:53 +0700

    update demo

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 var expres = require('express');
 var app = expres();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
-app.use(expres.static('public'));
-app.set('view engine', 'ejs');
+// import body-parser
+// Sử dụng body parser như một middleware
+// body parser lắng nghe và nhận dữ liệu từ các params truyền lên và trả về dữ liệu trong req.body
+var bodyParser = require('body-parser')
+// định dạng dữ liệu post lên server
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-server.listen(2000);
+app.listen(2000);
 
-io.on('connection', function (socket) {
-    //
+// Methods get
+// gửi request lên server url /tintuc/93
+app.get('/news/:id', function (req, res) {
+    // Trả về kết quả: Tin tức có id: 93
+    res.send('Tin tức có id: '+ req.params.id);
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html')
-});
 
-// app.get('/', function (req, res) {
-//    res.render('index');
-// });
+// Methods post
+// gửi request lên server url /user
+// trong body truyền lên 2 params username = Minh Dương và password = 123456
+app.post('/user', urlencodedParser, function (req, res) {
+    // Khai báo biến username = username trả về trong body 
+    let username = req.body.username;
+    // Khai báo biến pass = password trả về trong body 
+    let pass = req.body.password;
+    // Kết quả in ra Username: Minh Dương / Password: 123456
+    res.send('Username: ' + username + ' / ' + 'Password: ' + pass);
+})
