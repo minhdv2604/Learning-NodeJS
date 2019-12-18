7b117192cd2eedd18ac8ec3ef9f744dff34596ba
MinhDV <minhdv@ominext.com>
2019-12-18 17:23:53 +0700

    update

--- test-redis.js
+++ test-redis.js
@@ -6,16 +6,21 @@ const app = express();
 // create and connect redis client to local instance.
 const client = redis.createClient();
 
-// Print redis errors to the console
+// Lắng nghe sự kiện connect
 client.on('connect', function() {
+    // In ra Redis client connected
     console.log('Redis client connected');
 });
 
+// Lắng nghe sự kiện error
 client.on('error', function (err) {
+    // In ra Something went wrong + lỗi
     console.log('Something went wrong ' + err);
 });
 
+// set key 'my test key' có value 'my test value'
 client.set('my test key', 'my test value', redis.print);
+// get value từ key 'my test key'
 client.get('my test key', function (error, result) {
     if (error) {
         console.log(error);
