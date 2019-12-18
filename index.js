bc14acd9679a8a86a49e0c2ff14e50749020f675
MinhDV <minhdv@ominext.com>
2019-12-18 16:45:19 +0700

    update

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ var io = require('socket.io')(server);
 
 server.listen('8000');
 
+// bắt sự kiện kết nối từ client
 io.on('connection', function (socket) {
+    // lắng nghe sự kiện client-send-color gửi lên từ client
     socket.on('client-send-color', function (data) {
+        // truyền data cho tất cả các client đang kết nối
         io.emit('server-send-color', data);
     });
 });
